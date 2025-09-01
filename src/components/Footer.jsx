4e679a6f973843b84f5d5dd65f23fda064ee82f5
cezardhiandra <cezardhiandra@gmail.com>
2025-09-01 22:23:51 +0800

    Update tampilan footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,12 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <footer className="py-6 text-center text-gray-400 bg-black">
-      <p>Official Website | PT. Berkah Angsana</p>
+    <footer className="py-4 bg-white">
+      <div className="px-6 mx-auto max-w-7xl">
+        <p className="text-sm text-left text-gray-700 font-poppins">
+          Official Website | PT. Berkah Angsana
+        </p>
+      </div>
     </footer>
   );
 }
